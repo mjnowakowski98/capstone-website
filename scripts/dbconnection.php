8b8bb2742b38e68b64b9ac90405d445420cc4628
Matthew Nowakowski <mjnowakowski@email.neit.edu>
2019-02-11 13:59:42 -0500

    Resolved naming conflict between dbconnection and signUp()

--- scripts/dbconnection.php
+++ scripts/dbconnection.php
@@ -1,8 +1,8 @@
 <?php
     $dsn = "mysql:host=localhost;dbname=capstone;";
-    $userName="capstone";
-    $password="0000";
-    try { $db = new PDO($dsn, $userName, $password); }
+    $dbuserName="capstone";
+    $dbpassword="0000";
+    try { $db = new PDO($dsn, $dbuserName, $dbpassword); }
     catch(PDOException $e) {
         echo $e->getMessage();
         die("Can't connect to database");
